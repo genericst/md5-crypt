fc81b51d3f0b74e9917a5736a9743f6226d7ef27
Spronjee <i.chukhlebov6530@gmail.com>
2020-01-20 22:18:34 +0200

    Added choosing a methods of encrypting

--- main.js
+++ main.js
@@ -28,6 +28,20 @@ const chooseModeCallback = mode => {
   }
 };
 
+const chooseMethod = method => {
+  switch (method) {
+      case '1':
+      console.log('You choosen RSA method');break;
+      case '2':
+      console.log('You choosen md5 method');break;
+  
+      default: 
+      console.log('Incorrect input');
+  }
+  const entryMessage = 'Please, choose mode\n' +
+        '1 - Encrypt, 2 - Decrypt\nYour mode: ';
+    rl.question(entryMessage, chooseModeCallback);
+};
 
 // Entry Point //
 
@@ -47,8 +61,9 @@ const chooseModeCallback = mode => {
       console.log('Unknown command!');
     }
   } else {
-    const entryMessage = 'Please, choose mode\n' +
-        '1 - Encrypt, 2 - Decrypt\nYour mode: ';
-    rl.question(entryMessage, chooseModeCallback);
+    const entryMessage1 = 'Which method you would like to use?\n'+
+      '1 - RSA, 2 - md5\n Your mode: ';
+      rl.question(entryMessage1,chooseMethod);
+
   }
 })(process.argv[2], process.argv[3]);
